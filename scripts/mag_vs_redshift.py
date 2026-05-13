f4e93112f40de77b03fdbde78a4969c037edb67b
Leonard Lebrun <leonard.lebrun@physik.uzh.ch>
2026-05-13 12:34:22 +0200

    updated try cuts

--- scripts/mag_vs_redshift.py
+++ scripts/mag_vs_redshift.py
@@ -14,22 +14,27 @@ lrg_cut = ("LRG cut", apply_lrg_cuts)
 if __name__ == "__main__":
 
     run = RunDir("mag_vs_z")
+    redshift_col = "true_redshift_gal"
+    mag_col = "wise_w1_mag"
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--redshift-max", type=float, default=3.0,
                         help="Maximum redshift cut")
     parser.add_argument("--mag-max",      type=float, default=30,
                         help="Absolute magnitude cut on W! (e.g. 0)")
+    parser.add_argument("--mag-col", type=str, default=mag_col, 
+                        help="column for magnitude")
     args = parser.parse_args()
 
     redshift_max = args.redshift_max
     mag_max      = args.mag_max
+    mag_col = args.mag_col
 
     cut = lrg_cut
 
     h = Hist2D(
-        x         = pl.col("true_redshift_gal"),
-        y         = pl.col("wise_w1_mag"),
+        x         = pl.col(redshift_col),
+        y         = pl.col(mag_col),
         x_bins    = np.linspace(0, redshift_max, 201),
         y_bins    = np.linspace(14, mag_max, 201),
         x_label   = "redshift",
@@ -38,8 +43,8 @@ if __name__ == "__main__":
     ).compute()
 
     fig, ax = h.plot(title=cut[0])
-    run.save_plot(fig, f"hist2d_mag_vs_z_M{mag_max}_Z{redshift_max}")
+    run.save_plot(fig, f"hist2d_{mag_col}_vs_z_M{mag_max}_Z{redshift_max}")
 
-    h.save(run, name=f"h2D_mag_vs_z_M{mag_max}_Z{redshift_max}")
+    h.save(run, name=f"h2D_{mag_col}_vs_z_M{mag_max}_Z{redshift_max}")
 
     print("Done!")
